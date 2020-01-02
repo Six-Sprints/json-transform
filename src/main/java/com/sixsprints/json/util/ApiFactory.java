c3b62c93c941419e2f6eb33cf2dbaa9be1e743fc
Karan Gujral <karan@sixsprints.com>
2020-01-02 20:30:08 +0530

    added transform

--- src/main/java/com/sixsprints/json/util/ApiFactory.java
+++ src/main/java/com/sixsprints/json/util/ApiFactory.java
@@ -1,21 +1,30 @@
 package com.sixsprints.json.util;
 
+import java.io.IOException;
+
 import org.joda.time.DateTimeZone;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sixsprints.json.dto.Mapping;
+import com.sixsprints.json.dto.TransformerResponse;
+import com.sixsprints.json.exception.ApiException;
+import com.sixsprints.json.service.MappingService;
 
+import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class ApiFactory {
 
-  public static ObjectMapper defaultMapper() {
-    ObjectMapper mapper = new ObjectMapper();
+  private static final ObjectMapper mapper;
+
+  static {
+    mapper = new ObjectMapper();
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     mapper.setTimeZone(DateTimeZone.forID("+05:30").toTimeZone());
-    return mapper;
   }
 
   public static <T> T create(Class<T> clazz, String baseUrl, ObjectMapper mapper) {
@@ -28,7 +37,17 @@ public class ApiFactory {
   }
 
   public static <T> T create(Class<T> clazz, String baseUrl) {
-    return create(clazz, baseUrl, defaultMapper());
+    return create(clazz, baseUrl, mapper);
+  }
+
+  public static <T> T makeCallAndTransform(Call<String> call, Class<T> clazz, Mapping mapping)
+    throws IOException, ApiException {
+    Response<String> response = call.execute();
+    if (response.isSuccessful()) {
+      TransformerResponse convert = MappingService.convert(mapping, response.body());
+      return mapper.convertValue(convert.getOutput(), clazz);
+    }
+    throw ApiException.builder().response(response).error("Response was unsuccessfull").build();
   }
 
 }
